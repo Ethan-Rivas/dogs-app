8ac98697599aa72ff3b1c2e82c2d7709770d4e9e
Ethan-Rivas <ethan-rivas@hotmail.com>
2017-08-25 10:02:14 -0500

    ng-view

--- app/assets/javascripts/angular-app/application.js
+++ app/assets/javascripts/angular-app/application.js
@@ -1,15 +1,16 @@
 var app = angular.module('dogs', ['ngRoute']);
 
-app.config(function($routeProvider) {
-    $routeProvider
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {    
+    $routeProvider    
     .when("/", {
-        templateUrl : "templates/index.html"
+        templateUrl : "templates/index.html.hbs"
     })
     .when("/dogs", {
-        templateUrl : "templates/dogs.html.hbs",
-        controller : "TestController"
-      })
-  });
+        templateUrl : "templates/dogs.html.hbs"
+    })
+
+    $locationProvider.html5Mode(true);    
+}]);
 
 app.controller('TestController', function($scope) {
   $scope.message = "Hola";
