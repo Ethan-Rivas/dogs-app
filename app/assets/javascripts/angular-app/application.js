faf71be53e067c782adcb7ae9e3698f4214afbe4
Edengarde <head_shot_360@hotmail.com>
2017-08-25 09:26:41 -0500

    Changes on frontend & index

--- app/assets/javascripts/angular-app/application.js
+++ app/assets/javascripts/angular-app/application.js
@@ -1,4 +1,15 @@
-var app = angular.module('dogs', []);
+var app = angular.module('dogs', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/index.html"
+    })
+    .when("/dogs", {
+        templateUrl : "templates/dogs.html.hbs",
+        controller : "TestController"
+      })
+  });
 
 app.controller('TestController', function($scope) {
   $scope.message = "Hola";
