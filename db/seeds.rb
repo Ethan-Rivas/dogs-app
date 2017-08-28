d74f1e7a25f335ca34b21ed90356e4002a8267d8
Ethan Rivas <ethan-rivas@hotmail.com>
2017-08-28 08:31:56 -0500

    seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create(name:'Chloe',description:'Cute Doge',sex:'Fem',size:'Small')
+Vaccine.create(name:'Perfect Vac',description:'Good Vac')
